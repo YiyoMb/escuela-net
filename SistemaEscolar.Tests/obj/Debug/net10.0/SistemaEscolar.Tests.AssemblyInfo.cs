532052b29bf5daab9979ed83a510c6c54a9fabb6
YiyoMb <diegobalderas.864eyou@gmail.com>
2025-12-05 23:56:01 -0600

    feat: agregar archivo de solución para CI/CD

--- SistemaEscolar.Tests/obj/Debug/net10.0/SistemaEscolar.Tests.AssemblyInfo.cs
+++ SistemaEscolar.Tests/obj/Debug/net10.0/SistemaEscolar.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaEscolar.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0824deecae37449531b9a10777ef1fc9f21fea22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7af0e8c5096fc56674c131a48ad071fd764a132")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaEscolar.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaEscolar.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
